47152d3850a401694ec01dd507f2e3a3928b8320
mateus <mateusinfo36@gmail.com>
2023-03-17 17:02:22 -0300

    Primeiro commit

---
+++ src/main/java/Operadores/Main.java
@@ -0,0 +1,11 @@
+package Operadores;
+
+public class Main {
+        int idade = 5;
+        float cotacaoDolar = 5.0f;
+        double cotacaoEuro = 6.0d;
+        char genero = 'M';
+        byte placar =  0;
+        boolean estaCadastrado = false;
+        String nome = "teste";
+}
